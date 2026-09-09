5e5d87ef475f8d505a43607f41d461e1af58e007
Wilbert Ribeiro <wkelyson@gmail.com>
2026-09-09 19:21:39 -0300

    feat: refactor connection logic to streamline user verification and add connection specs

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,14 +3,14 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      set_current_user || reject_unauthorized_connection
+      self.current_user = find_verified_user
     end
 
     private
-      def set_current_user
-        if session = Session.find_by(id: cookies.signed[:session_id])
-          self.current_user = session.user
-        end
-      end
+
+    def find_verified_user
+      session = Session.find_by(id: cookies.signed[:session_id])
+      session&.user || reject_unauthorized_connection
+    end
   end
 end
